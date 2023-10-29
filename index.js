d8f5ab33961fb74485d93bc393219d01fa2c5428
danishirfan21 <44131991+danishirfan21@users.noreply.github.com>
2023-10-29 21:40:10 +0500

    updated

--- index.js
+++ index.js
@@ -33,27 +33,29 @@ const postsContainerDiv = document.getElementById("posts-container");
 
 posts.forEach(e => {
     const postDiv = document.createElement("div");
-    postDiv.classList.add("bio");
-    postsContainerDiv.innerHTML += `
-        <div class="bio">
-            <img class="user-icon" src=${e?.avatar}>
-            <div class="name-location">
-                <p class="name">${e?.name}</p>
-                <p class="location">${e?.location}</p>    
-            </div>        
-        </div>
-            
-        <img style="width: 100%;" src=${e?.post}>
-            
-        <div class="all-icons">
-            <img src="images/icon-heart.png">
-            <img src="images/icon-comment.png">
-            <img src="images/icon-dm.png">
-                
-            <p class="likes">${e?.likes} likes</p>
-                
-            <p class="username-caption"><span class="name">${e?.username}</span> ${e?.comment}</p>
-        </div>
-    `
+    postDiv.innerHTML += `
+   <div class="bio">
+       <img class="user-icon" src="${e?.avatar}">
+       <div class="name-location">
+           <p class="name">${e?.name}</p>
+           <p class="location">${e?.location}</p>
+       </div>
+   </div>
+         
+   <img style="width: 100%;" src="${e?.post}">
+         
+   <div class="all-icons">
+       <img src="images/icon-heart.png">
+       <img src="images/icon-comment.png">
+       <img src="images/icon-dm.png">
+           
+       <p class="likes">${e?.likes} likes</p>
+           
+       <p class="username-caption"><span class="name">${e?.username}</span> ${e?.comment}</p>
+   </div>
+`;
+
+postsContainerDiv.appendChild(postDiv);
+
     
 })
